65546cd113c486d50104549741bedb4fd7b646aa
Ms.Bithi <bithimony01904@gmail.com>
2024-11-06 16:33:31 +0600

    update another problem

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
-//Arithmetic Operation by Object
+// Object Manipulation
+const student = {
+   name: "Mitu",
+   age: 20,
+   grades: [90, 60, 80, 70],
+   averageGrades: function() {
+      let sum = 0;
+      for (let i = 0; i < this.grades.length; i++) {
+         sum += this.grades[i];
+      }
+      console.log(sum / this.grades.length); // Dividing by the number of grades
+   }
+};
 
-
-function Arithmetic_Operation_by_Object(X,Y)
-{
-    return {
-     Sum : function()
-     {
-        return X + Y;
-     },
-     Sub : function()
-     {
-        return X-Y;
-     },
-     Mul : function()
-     {
-        return X*Y;
-     }
-    };
-}
-const ArithmeticOperation = Arithmetic_Operation_by_Object(50,10);
-console.log(ArithmeticOperation.Sum());
+// Call the method to calculate and print the average grade
+student.averageGrades();

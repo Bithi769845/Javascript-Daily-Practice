8eca5b90794609a91be427587a931c7345fd5765
Ms.Bithi <bithimony01904@gmail.com>
2024-11-07 12:32:40 +0600

    3rd day problem(Array methods and sorting)

--- script.js
+++ script.js
@@ -1,16 +1,56 @@
-// Object Manipulation
-const student = {
-   name: "Mitu",
-   age: 20,
-   grades: [90, 60, 80, 70],
-   averageGrades: function() {
-      let sum = 0;
-      for (let i = 0; i < this.grades.length; i++) {
-         sum += this.grades[i];
-      }
-      console.log(sum / this.grades.length); // Dividing by the number of grades
+// Array Methods and Sorting
+const books = [
+   {
+      title: 'Machine Learning',
+      author: 'Shumi Begum',
+      yearPublished: 1996
+   },
+   {
+      title: 'Deep Learning',
+      author: 'Mitu Rahman',
+      yearPublished: 2001
+   },
+   {
+      title: 'Adversarial Attacks in Machine Learning',
+      author: 'Hashina Akter',
+      yearPublished: 1994
+   },
+   {
+      title: 'Cybersecurity Essentials',
+      author: 'Khadija Khan',
+      yearPublished:2000 
+   },
+   {
+      title: 'DevOps Principles and Practices',
+      author: 'Kulshum Jahan',
+      yearPublished: 1995
    }
-};
+];
 
-// Call the method to calculate and print the average grade
-student.averageGrades();
+//use builtin function
+//const sortedBooks = books.sort((a, b) => b.yearPublished - a.yearPublished);
+// by logic building
+for (let i = 0; i < books.length - 1; i++) {
+   for (let j = 0; j < books.length - 1 - i; j++) {
+      if (books[j].yearPublished > books[j + 1].yearPublished) {
+         // Swap elements
+         let temp = books[j];
+         books[j] = books[j + 1];
+         books[j + 1] = temp;
+      }
+   }
+  
+}
+
+//use built_in Function
+//const filter = books.filter(books => books.yearPublished < 2000);
+//by logic building
+const filter = [];
+for(let i = 0 ; i<books.length;i++){
+   if(books[i].yearPublished<2000)
+   {
+      filter.push(books[i]);
+   }
+}
+console.log(filter);
+

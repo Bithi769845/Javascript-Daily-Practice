a642e22df7b29ad7fdda362f2c016f6a9c628771
Ms.Bithi <bithimony01904@gmail.com>
2024-11-07 14:44:40 +0600

    another problem on string manupulation

--- script.js
+++ script.js
@@ -1,56 +1,20 @@
-// Array Methods and Sorting
-const books = [
-   {
-      title: 'Machine Learning',
-      author: 'Shumi Begum',
-      yearPublished: 1996
-   },
-   {
-      title: 'Deep Learning',
-      author: 'Mitu Rahman',
-      yearPublished: 2001
-   },
-   {
-      title: 'Adversarial Attacks in Machine Learning',
-      author: 'Hashina Akter',
-      yearPublished: 1994
-   },
-   {
-      title: 'Cybersecurity Essentials',
-      author: 'Khadija Khan',
-      yearPublished:2000 
-   },
-   {
-      title: 'DevOps Principles and Practices',
-      author: 'Kulshum Jahan',
-      yearPublished: 1995
-   }
-];
+// String Manipulation
 
-//use builtin function
-//const sortedBooks = books.sort((a, b) => b.yearPublished - a.yearPublished);
-// by logic building
-for (let i = 0; i < books.length - 1; i++) {
-   for (let j = 0; j < books.length - 1 - i; j++) {
-      if (books[j].yearPublished > books[j + 1].yearPublished) {
-         // Swap elements
-         let temp = books[j];
-         books[j] = books[j + 1];
-         books[j + 1] = temp;
-      }
-   }
-  
+function reverse_sentence(Sentence){
+str = "";
+
+for(let i = Sentence.length-1; i>=0;i--){
+   str += Sentence[i];
+}
+return str;
 }
 
-//use built_in Function
-//const filter = books.filter(books => books.yearPublished < 2000);
-//by logic building
-const filter = [];
-for(let i = 0 ; i<books.length;i++){
-   if(books[i].yearPublished<2000)
-   {
-      filter.push(books[i]);
-   }
+let Sentence = "Hello world";
+let reversed = reverse_sentence(Sentence); 
+let words = reversed.split(" ");
+let remainreverse = "";
+for(let i = words.length-1; i>=0;i--){
+   remainreverse += words[i]+ " ";
 }
-console.log(filter);
 
+console.log(remainreverse.trim());

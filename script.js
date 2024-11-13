7efb19cb158003c670fe6d9d6a084e80c4bdb84c
Ms.Bithi <bithimony01904@gmail.com>
2024-11-13 14:38:24 +0600

    Template Strings and Expressions

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-function isPalindrome(num) {
-   // Initialize reverse based on the type of num
-   let reverse = typeof num === 'bigint' ? BigInt(0) : 0;
-   let temp = num;
+//  Template Strings and Expressions
 
-   while (temp > 0) {
-      // If num is BigInt, we use BigInt(10); otherwise, just 10 for Number
-      const base = typeof num === 'bigint' ? BigInt(10) : 10;
-      const rem = temp % base;
-      reverse = reverse * base + rem;
-      temp = temp / base;
 
-      // For regular numbers, use Math.floor to handle integer division
-      if (typeof num !== 'bigint') temp = Math.floor(temp);
-   }
-
-   if (num === reverse) {
-      console.log("It is a palindrome number");
-   } else {
-      console.log("It is not a palindrome");
-   }
+function Accept(name,age){
+     let greeting  = `Hello ${name}, you are ${age} years old!`;
+     return greeting;
 }
 
-// Testing the function with both Numbers and BigInt
-isPalindrome(121);               
-isPalindrome(123);              
-isPalindrome(BigInt(12321));      
-isPalindrome(BigInt(123456));     
+console.log(Accept("Ms.Bithi",25));
